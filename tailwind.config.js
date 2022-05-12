44e026fc8e3ac921720f186fd743e538396cc2ac
Canberk <canberkkaya06@gmail.com>
2022-05-12 17:34:49 +0300

    tailwind css setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        sky: colors.sky,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        rose: colors.rose,
+      },
+      fontFamily: {
+        inter: ['"Inter"', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
+}
